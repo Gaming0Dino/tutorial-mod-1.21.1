1fc0d9959ccd4c497346c8aae9b525ce01d7b59b
Jack <gamingjackmof@outlook.com>
2025-11-22 11:14:56 +0000

    Initial commit

---
+++ src/main/java/net/gaming0dino/tutorialmod/TutorialMod.java
@@ -0,0 +1,19 @@
+package net.gaming0dino.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.gaming0dino.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorial-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+        LOGGER.info("Loading Tutorial Mod...");
+
+        ModItems.registerModItems();
+	}
+}
